a1038f558bc4132487a433bb40a020331fa48e8f
awaken-psy <1036840137@qq.com>
2025-10-04 17:16:10 +0800

    lecture3 hw

---
+++ lecture2/build/CMakeFiles/split_channels.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for split_channels.
+# This may be replaced when dependencies are built.
--- lecture3/part1/main.cpp
+++ lecture3/part1/main.cpp
@@ -3,17 +3,39 @@ using namespace std;
 
 class Material
 {
+public:
+    Material();
+    ~Material();
+    void print();
 
 };
 
-// int main()
-// {
-//     cout << "--- 构造函数 ---" << endl;
-//     Material m;
+// 实现构造函数
+Material::Material()
+{
+    cout << "Material构造函数被调用" << endl;
+}
 
-//     cout << "\n--- print函数 ---" << endl;
-//     m.print();
+// 实现析构函数
+Material::~Material()
+{
+    cout << "Material析构函数被调用" << endl;
+}
 
-//     cout << "\n--- 自动进行析构函数 ---" << endl;
-//     return 0;
-// }
+// 实现print函数
+void Material::print()
+{
+    cout << "Material print函数被调用" << endl;
+}
+
+int main()
+{
+    cout << "--- 构造函数 ---" << endl;
+    Material m;
+
+    cout << "\n--- print函数 ---" << endl;
+    m.print();
+
+    cout << "\n--- 自动进行析构函数 ---" << endl;
+    return 0;
+}
